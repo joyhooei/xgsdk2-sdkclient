44dcfa60f98b419f9ba9b769d7b080e5fd6283ac
liujie4 <liujie4@kingsoft.com>
2015-08-05 11:32:02 +0800

    Add Pay Status

--- xgsdk-core/src/com/xgsdk/client/core/service/PayStatus.java
+++ xgsdk-core/src/com/xgsdk/client/core/service/PayStatus.java
@@ -1,14 +1,52 @@
 
 package com.xgsdk.client.core.service;
 
-public class PayStatus {
+public enum PayStatus {
 
-    public static final int STATUS_INITIAL = -1;
-    public static final int STATUS_WAITTING_CHANNEL_NOTIFY = 0;
-    public static final int STATUS_CANCELED = 99;
-    public static final int STATUS_PAY_FAIL_WAITTING_NOTIFY_GAME = 6;
-    public static final int STATUS_PAY_SUCCESS_WAITING_NOTIFY_GAME = 1;
-    public static final int STATUS_PAY_FAIL_NOTIFIED_GAME = 3;
-    public static final int STATUS_PAY_SUCCESS_NOTIFIED_GAME = 2;
-    public static final int STATUS_NOTIFY_GAME_FAIL = 5;
+    UNKNOWN(-99), INITIAL(-1), WAITTING_CHANNEL_NOTIFY(0), CANCELED(99), PAY_FAIL_WAITTING_NOTIFY_GAME(
+            6), PAY_SUCCESS_WAITING_NOTIFY_GAME(1), PAY_FAIL_NOTIFIED_GAME(3), PAY_SUCCESS_NOTIFIED_GAME(
+            2), NOTIFY_GAME_FAIL(5);
+
+    private int value;
+
+    private PayStatus(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public static PayStatus valueOf(int value) {
+        PayStatus status = null;
+        switch (value) {
+            case -1:
+                status = INITIAL;
+                break;
+            case 0:
+                status = WAITTING_CHANNEL_NOTIFY;
+                break;
+            case 99:
+                status = CANCELED;
+            case 1:
+                status = PAY_SUCCESS_WAITING_NOTIFY_GAME;
+                break;
+            case 2:
+                status = PAY_SUCCESS_NOTIFIED_GAME;
+                break;
+            case 3:
+                status = PAY_FAIL_NOTIFIED_GAME;
+                break;
+            case 5:
+                status = NOTIFY_GAME_FAIL;
+                break;
+            case 6:
+                status = PAY_FAIL_WAITTING_NOTIFY_GAME;
+                break;
+            case -99:
+            default:
+                status = UNKNOWN;
+        }
+        return status;
+    }
 }
