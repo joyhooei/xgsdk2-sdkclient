fb9a2a392b3f280d47b4e69983c032f8551510f4
asAngus <weipeng.angus@gmail.com>
2015-08-06 20:44:13 +0800

    修改http请求模式

--- xgsdk-demo/src/com/xgsdk/client/demo/orders/OrdersActivity.java
+++ xgsdk-demo/src/com/xgsdk/client/demo/orders/OrdersActivity.java
@@ -1,12 +1,8 @@
-
 package com.xgsdk.client.demo.orders;
 
-import com.xgsdk.client.core.service.PayService;
-import com.xgsdk.client.core.service.PayStatus;
-import com.xgsdk.client.core.service.Result;
-import com.xgsdk.client.core.utils.XGLog;
-import com.xgsdk.client.demo.GameInfo;
-import com.xgsdk.client.demo.utils.RUtil;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,178 +22,199 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Set;
+import com.xgsdk.client.core.service.ICallback;
+import com.xgsdk.client.core.service.PayService;
+import com.xgsdk.client.core.service.PayStatus;
+import com.xgsdk.client.core.service.Result;
+import com.xgsdk.client.core.utils.XGLog;
+import com.xgsdk.client.demo.GameInfo;
+import com.xgsdk.client.demo.utils.RUtil;
 
 public class OrdersActivity extends Activity {
 
-    private ListView mLVOrders;
-    private Button mBtnClean;
-    private SimpleAdapter mAdapter;
-    
-    private static final int MSG_REFRESH = 1000;
+	private ListView mLVOrders;
+	private Button mBtnClean;
+	private SimpleAdapter mAdapter;
 
-    private Handler mHandler = new Handler() {
-        public void handleMessage(android.os.Message msg) {
-            switch(msg.what){
-                case MSG_REFRESH:
-                    mAdapter.notifyDataSetChanged();
-                    break;
-            }
-        };
-    };
+	private static final int MSG_REFRESH = 1000;
 
-    private ArrayList<HashMap<String, Object>> mOrderList = new ArrayList<HashMap<String, Object>>();
+	private Handler mHandler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case MSG_REFRESH:
+				mAdapter.notifyDataSetChanged();
+				break;
+			}
+		};
+	};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(RUtil.getLayout(getApplicationContext(),
-                "xg_demo_activity_orders"));
-        mLVOrders = (ListView) findViewById(RUtil.getId(
-                getApplicationContext(), "xg_lv_orders"));
-        mLVOrders.setOnItemClickListener(new OnItemClickListener() {
+	private ArrayList<HashMap<String, Object>> mOrderList = new ArrayList<HashMap<String, Object>>();
 
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                    int position, long id) {
-                if (position < 1) {
-                    return;
-                }
-                HashMap<String, Object> map = mOrderList.get(position - 1);
-                if (map != null) {
-                    String orderId = (String) map.get(OrderUtils.KEY_ORDER_ID);
-                    if (!TextUtils.isEmpty(orderId)) {
-                        try {
-                            Result result = PayService
-                                    .queryOrderStatusInThread(
-                                            OrdersActivity.this, orderId);
-                            if (result != null && result.getData() != null) {
-                                JSONObject json = new JSONObject(result
-                                        .getData());
-                                String status = json.optString("status");
-                                if (!TextUtils.isEmpty(status)) {
-                                    PayStatus st = PayStatus.valueOf(Integer
-                                            .valueOf(status));
-                                    XGLog.d("query order status." + st);
-                                    OrderUtils.updateOrderStatus(
-                                            getApplicationContext(), GameInfo
-                                                    .getInstance().getUid(),
-                                            orderId, st.name());
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(RUtil.getLayout(getApplicationContext(),
+				"xg_demo_activity_orders"));
+		mLVOrders = (ListView) findViewById(RUtil.getId(
+				getApplicationContext(), "xg_lv_orders"));
+		mLVOrders.setOnItemClickListener(new OnItemClickListener() {
 
-                                    new LoadOrdersTask().execute();
-                                }
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (position < 1) {
+					return;
+				}
+				HashMap<String, Object> map = mOrderList.get(position - 1);
+				if (map != null) {
+					String orderId = (String) map.get(OrderUtils.KEY_ORDER_ID);
+					if (!TextUtils.isEmpty(orderId)) {
+						try {
+							PayService.queryOrderStatus(OrdersActivity.this,
+									orderId, new ICallback() {
 
-            }
-        });
-        mBtnClean = (Button) findViewById(RUtil.getId(getApplicationContext(),
-                "xg_btn_clean"));
+										@Override
+										public void callback(Result result,
+												String retStr) {
+											if (result != null
+													&& result.getData() != null) {
+												JSONObject json;
+												try {
+													json = new JSONObject(
+															result.getData());
+													String status = json
+															.optString("status");
+													if (!TextUtils
+															.isEmpty(status)) {
+														PayStatus st = PayStatus
+																.valueOf(Integer
+																		.valueOf(status));
+														XGLog.d("query order status."
+																+ st);
+														OrderUtils
+																.updateOrderStatus(
+																		getApplicationContext(),
+																		GameInfo.getInstance()
+																				.getUid(),
+																		result.getOrderId(),
+																		st.name());
 
-        mBtnClean.setOnClickListener(new OnClickListener() {
+														new LoadOrdersTask()
+																.execute();
+													}
+												} catch (JSONException e) {
+													e.printStackTrace();
+												}
 
-            @Override
-            public void onClick(View v) {
-                showCleanDialog();
-            }
-        });
+											}
+										}
+									});
 
-        mAdapter = new SimpleAdapter(this, mOrderList, RUtil.getLayout(
-                getApplicationContext(), "xg_demo_item_order"),
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
 
-        new String[] {
-                OrderUtils.KEY_ORDER_ID, OrderUtils.KEY_ORDER_DETAILS,
-                OrderUtils.KEY_ORDER_TIME, OrderUtils.KEY_ORDER_STATUS
-        },
+			}
+		});
+		mBtnClean = (Button) findViewById(RUtil.getId(getApplicationContext(),
+				"xg_btn_clean"));
 
-        new int[] {
-                RUtil.getId(getApplicationContext(), "xg_order_id"),
-                RUtil.getId(getApplicationContext(), "xg_order_details"),
-                RUtil.getId(getApplicationContext(), "xg_order_time"),
-                RUtil.getId(getApplicationContext(), "xg_order_status")
-        });
+		mBtnClean.setOnClickListener(new OnClickListener() {
 
-        mLVOrders.setAdapter(mAdapter);
-        View header = getLayoutInflater()
-                .inflate(
-                        RUtil.getLayout(getApplicationContext(),
-                                "xg_demo_orders_title"), null);
-        mLVOrders.addHeaderView(header);
-        new LoadOrdersTask().execute();
-    }
+			@Override
+			public void onClick(View v) {
+				showCleanDialog();
+			}
+		});
 
-    private class LoadOrdersTask extends
-            AsyncTask<Void, Integer, HashMap<String, JSONObject>> {
+		mAdapter = new SimpleAdapter(this, mOrderList, RUtil.getLayout(
+				getApplicationContext(), "xg_demo_item_order"),
 
-        @Override
-        protected HashMap<String, JSONObject> doInBackground(Void... params) {
-            return OrderUtils.getOrders(OrdersActivity.this, GameInfo
-                    .getInstance().getUid());
-        }
+		new String[] { OrderUtils.KEY_ORDER_ID, OrderUtils.KEY_ORDER_DETAILS,
+				OrderUtils.KEY_ORDER_TIME, OrderUtils.KEY_ORDER_STATUS },
 
-        @Override
-        protected void onPostExecute(HashMap<String, JSONObject> result) {
-            super.onPostExecute(result);
-            mOrderList.clear();
-            if (result != null && !result.isEmpty()) {
-                Set<String> set = result.keySet();
-                for (String orderid : set) {
-                    try {
-                        JSONObject json = result.get(orderid);
-                        HashMap<String, Object> map = new HashMap<String, Object>();
-                        map.put(OrderUtils.KEY_ORDER_ID, orderid);
-                        // map.put(OrderUtils.KEY_ORDER_DETAILS,
-                        // json.get(OrderUtils.KEY_ORDER_DETAILS));
-                        map.put(OrderUtils.KEY_ORDER_DETAILS, null);
-                        map.put(OrderUtils.KEY_ORDER_TIME,
-                                json.get(OrderUtils.KEY_ORDER_TIME));
-                        map.put(OrderUtils.KEY_ORDER_STATUS,
-                                json.get(OrderUtils.KEY_ORDER_STATUS));
-                        mOrderList.add(map);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
+		new int[] { RUtil.getId(getApplicationContext(), "xg_order_id"),
+				RUtil.getId(getApplicationContext(), "xg_order_details"),
+				RUtil.getId(getApplicationContext(), "xg_order_time"),
+				RUtil.getId(getApplicationContext(), "xg_order_status") });
 
-                }
-                mAdapter.notifyDataSetChanged();
-            } else {
+		mLVOrders.setAdapter(mAdapter);
+		View header = getLayoutInflater()
+				.inflate(
+						RUtil.getLayout(getApplicationContext(),
+								"xg_demo_orders_title"), null);
+		mLVOrders.addHeaderView(header);
+		new LoadOrdersTask().execute();
+	}
 
-            }
-        }
-    }
+	private class LoadOrdersTask extends
+			AsyncTask<Void, Integer, HashMap<String, JSONObject>> {
 
-    private void showCleanDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(RUtil.getString(getApplicationContext(),
-                "xg_clean_msg"));
-        builder.setPositiveButton(
-                RUtil.getString(getApplicationContext(), "xg_ok"),
-                new DialogInterface.OnClickListener() {
+		@Override
+		protected HashMap<String, JSONObject> doInBackground(Void... params) {
+			return OrderUtils.getOrders(OrdersActivity.this, GameInfo
+					.getInstance().getUid());
+		}
 
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        OrderUtils.cleanOrders(getApplicationContext(),
-                                GameInfo.getInstance().getUid());
-                        mOrderList.clear();
-                        mAdapter.notifyDataSetChanged();
-                        dialog.dismiss();
-                    }
-                });
+		@Override
+		protected void onPostExecute(HashMap<String, JSONObject> result) {
+			super.onPostExecute(result);
+			mOrderList.clear();
+			if (result != null && !result.isEmpty()) {
+				Set<String> set = result.keySet();
+				for (String orderid : set) {
+					try {
+						JSONObject json = result.get(orderid);
+						HashMap<String, Object> map = new HashMap<String, Object>();
+						map.put(OrderUtils.KEY_ORDER_ID, orderid);
+						// map.put(OrderUtils.KEY_ORDER_DETAILS,
+						// json.get(OrderUtils.KEY_ORDER_DETAILS));
+						map.put(OrderUtils.KEY_ORDER_DETAILS, null);
+						map.put(OrderUtils.KEY_ORDER_TIME,
+								json.get(OrderUtils.KEY_ORDER_TIME));
+						map.put(OrderUtils.KEY_ORDER_STATUS,
+								json.get(OrderUtils.KEY_ORDER_STATUS));
+						mOrderList.add(map);
+					} catch (JSONException e) {
+						e.printStackTrace();
+					}
 
-        builder.setNegativeButton(
-                RUtil.getString(getApplicationContext(), "xg_cancel"),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        builder.create().show();
-    }
+				}
+				mAdapter.notifyDataSetChanged();
+			} else {
+
+			}
+		}
+	}
+
+	private void showCleanDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(RUtil.getString(getApplicationContext(),
+				"xg_clean_msg"));
+		builder.setPositiveButton(
+				RUtil.getString(getApplicationContext(), "xg_ok"),
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						OrderUtils.cleanOrders(getApplicationContext(),
+								GameInfo.getInstance().getUid());
+						mOrderList.clear();
+						mAdapter.notifyDataSetChanged();
+						dialog.dismiss();
+					}
+				});
+
+		builder.setNegativeButton(
+				RUtil.getString(getApplicationContext(), "xg_cancel"),
+				new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.dismiss();
+					}
+				});
+		builder.create().show();
+	}
 }
--- xgsdk-demo/src/com/xgsdk/client/demo/utils/AuthUtil.java
+++ xgsdk-demo/src/com/xgsdk/client/demo/utils/AuthUtil.java
@@ -1,71 +1,55 @@
-
 package com.xgsdk.client.demo.utils;
-
-import com.xgsdk.client.api.entity.XGUser;
-import com.xgsdk.client.core.service.AuthService;
-import com.xgsdk.client.core.service.Result;
-import com.xgsdk.client.demo.GameInfo;
-
-import org.json.JSONObject;
 
 import android.app.Activity;
 
+import com.xgsdk.client.core.service.AuthService;
+import com.xgsdk.client.core.service.ICallback;
+
 public class AuthUtil {
-    private static final String TAG = "TestUtil";
 
-    public static XGUser auth(Activity activity, String authInfo) {
-        XGUser user = new XGUser();
-        try {
-            Result result = AuthService.sessionAuthInThread(activity, authInfo);
-            if (result != null) {
-                JSONObject jsonData = new JSONObject(result.getData());
-                String uid = jsonData.optString("uId");
-                GameInfo.getInstance().setUid(uid);
-                user.setAuthInfo(authInfo);
-                user.setUid(uid);
-                ToastUtil.showToast(activity, result.getData());
+	public static void auth(final Activity activity, final String authInfo,
+			ICallback callback) {
+		try {
+			AuthService.sessionAuth(activity, authInfo, callback);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return user;
-    }
+	// private static void sendRequestWithHttpClient(final Activity activity,
+	// final String url) {
+	// new Thread(new Runnable() {
+	//
+	// @Override
+	// public void run() {
+	// HttpClient httpCient = new DefaultHttpClient();
+	// HttpGet httpGet = new HttpGet(url);
+	// Log.d(TAG, url);
+	//
+	// try {
+	// HttpResponse httpResponse = httpCient.execute(httpGet);
+	// if (httpResponse.getStatusLine().getStatusCode() == 200) {
+	// HttpEntity entity = httpResponse.getEntity();
+	// final String response = EntityUtils.toString(entity,
+	// "utf-8");
+	//
+	// Log.w(TAG, "auth = " + response);
+	// activity.runOnUiThread(new Runnable() {
+	//
+	// @Override
+	// public void run() {
+	// ToastUtil.showToast(activity, response);
+	//
+	// }
+	// });
+	// }
+	//
+	// } catch (Exception e) {
+	// Log.e(TAG, "auth error : ", e);
+	// }
+	//
+	// }
+	// }).start();
 
-    // private static void sendRequestWithHttpClient(final Activity activity,
-    // final String url) {
-    // new Thread(new Runnable() {
-    //
-    // @Override
-    // public void run() {
-    // HttpClient httpCient = new DefaultHttpClient();
-    // HttpGet httpGet = new HttpGet(url);
-    // Log.d(TAG, url);
-    //
-    // try {
-    // HttpResponse httpResponse = httpCient.execute(httpGet);
-    // if (httpResponse.getStatusLine().getStatusCode() == 200) {
-    // HttpEntity entity = httpResponse.getEntity();
-    // final String response = EntityUtils.toString(entity,
-    // "utf-8");
-    //
-    // Log.w(TAG, "auth = " + response);
-    // activity.runOnUiThread(new Runnable() {
-    //
-    // @Override
-    // public void run() {
-    // ToastUtil.showToast(activity, response);
-    //
-    // }
-    // });
-    // }
-    //
-    // } catch (Exception e) {
-    // Log.e(TAG, "auth error : ", e);
-    // }
-    //
-    // }
-    // }).start();
-
-    // }
+	// }
 }
