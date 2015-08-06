917b1a34a14e21b7f8381d551ff492e9b2b5f213
asAngus <weipeng.angus@gmail.com>
2015-08-06 20:51:13 +0800

    修改http请求模式

--- xgsdk-testchannel/src/com/xgsdk/client/testchannel/view/PayDialog.java
+++ xgsdk-testchannel/src/com/xgsdk/client/testchannel/view/PayDialog.java
@@ -1,12 +1,4 @@
-
 package com.xgsdk.client.testchannel.view;
-
-import com.xgsdk.client.api.XGErrorCode;
-import com.xgsdk.client.api.callback.PayCallBack;
-import com.xgsdk.client.api.entity.PayInfo;
-import com.xgsdk.client.core.service.PayService;
-import com.xgsdk.client.core.utils.XGLog;
-import com.xgsdk.client.testchannel.util.CommonStr;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -16,64 +8,71 @@ import android.content.DialogInterface.OnClickListener;
 import android.view.Gravity;
 import android.widget.TextView;
 
+import com.xgsdk.client.api.XGErrorCode;
+import com.xgsdk.client.api.callback.PayCallBack;
+import com.xgsdk.client.api.entity.PayInfo;
+import com.xgsdk.client.core.service.PayService;
+import com.xgsdk.client.core.utils.XGLog;
+import com.xgsdk.client.testchannel.util.CommonStr;
+
 public class PayDialog {
 
-    public PayDialog() {
+	public PayDialog() {
 
-    }
+	}
 
-    public void showDialog(final Activity activity, final PayInfo payment,
-            final PayCallBack payCallBack) {
-        AlertDialog.Builder builder = new Builder(activity);
-        TextView title = new TextView(activity);
-        title.setText(CommonStr.PAY_ORDER);
-        title.setGravity(Gravity.CENTER_HORIZONTAL);
-        title.setTextSize(22);
-        builder.setCustomTitle(title);
-        final AlertDialog payDialog = builder.create();
-        OrderDetailLayout orderLayout = new OrderDetailLayout(activity, payment);
-        builder.setView(orderLayout);
-        builder.setPositiveButton(CommonStr.PAY_SUCCESS, new OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // TODO Auto-generated method stub
-                payCallBack.onSuccess("pay success!");
+	public void showDialog(final Activity activity, final PayInfo payment,
+			final PayCallBack payCallBack) {
+		AlertDialog.Builder builder = new Builder(activity);
+		TextView title = new TextView(activity);
+		title.setText(CommonStr.PAY_ORDER);
+		title.setGravity(Gravity.CENTER_HORIZONTAL);
+		title.setTextSize(22);
+		builder.setCustomTitle(title);
+		final AlertDialog payDialog = builder.create();
+		OrderDetailLayout orderLayout = new OrderDetailLayout(activity, payment);
+		builder.setView(orderLayout);
+		builder.setPositiveButton(CommonStr.PAY_SUCCESS, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				payCallBack.onSuccess("pay success!");
 
-                try {
-                    String xgOrderId = payment.getXgOrderId();
-                    PayService.testChannelNotifyInThread(activity, xgOrderId);
+				try {
+					String xgOrderId = payment.getXgOrderId();
+					PayService.testChannelNotify(activity, xgOrderId, null);
 
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            }
-        });
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
 
-        builder.setNeutralButton(CommonStr.PAY_CANCEL, new OnClickListener() {
+		builder.setNeutralButton(CommonStr.PAY_CANCEL, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String xgOrderId = payment.getXgOrderId();
-                try {
-                    PayService.cancelOrderInThread(activity, xgOrderId);
-                } catch (Exception e) {
-                    XGLog.e("cancel order error", e);
-                }
-                payCallBack.onCancel("pay cancel!");
-                payDialog.dismiss();
-            }
-        });
-        builder.setNegativeButton(CommonStr.PAY_FAIL, new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String xgOrderId = payment.getXgOrderId();
+				try {
+					PayService.cancelOrder(activity, xgOrderId, null);
+				} catch (Exception e) {
+					XGLog.e("cancel order error", e);
+				}
+				payCallBack.onCancel("pay cancel!");
+				payDialog.dismiss();
+			}
+		});
+		builder.setNegativeButton(CommonStr.PAY_FAIL, new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // TODO Auto-generated method stub
-                payCallBack
-                        .onFail(XGErrorCode.OTHER_ERROR, "sorry,pay failed!");
-            }
-        });
-        builder.show();
-    }
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				payCallBack
+						.onFail(XGErrorCode.OTHER_ERROR, "sorry,pay failed!");
+			}
+		});
+		builder.show();
+	}
 
 }
