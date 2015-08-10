00413d9ea036d9663689cf7a73463e0e7d1d8065
sevenmagic <hmsaide@gmail.com>
2015-08-10 20:43:35 +0800

    Type: update Desc: xgsdk-data-v2.0 protocal update

--- xgsdk-api/src/com/xgsdk/client/api/statistics/Statistics.java
+++ xgsdk-api/src/com/xgsdk/client/api/statistics/Statistics.java
@@ -1,12 +1,6 @@
 
 package com.xgsdk.client.api.statistics;
 
-import com.xgsdk.client.api.callback.ExitCallBack;
-import com.xgsdk.client.api.callback.PayCallBack;
-import com.xgsdk.client.api.entity.GameServerInfo;
-import com.xgsdk.client.api.entity.PayInfo;
-import com.xgsdk.client.api.entity.RoleInfo;
-import com.xgsdk.client.api.entity.XGUser;
 import com.xgsdk.client.core.data.XGDataMonitor;
 import com.xgsdk.client.core.utils.XGLog;
 
@@ -19,6 +13,8 @@ public class Statistics {
     private static final String DATA_CLASS = "com.xgsdk.client.data.XGDataAgent";
     private static XGDataMonitor sDataAgent = null;
 
+    //******************** 生命周期 ********************//
+
     public static void onCreate(Activity activity) {
         try {
             sDataAgent = (XGDataMonitor) Class.forName(DATA_CLASS)
@@ -30,127 +26,425 @@ public class Statistics {
         }
     }
 
-    public static void onResume(Activity activity) {
+    public static void onResume(Activity activity, Object ext) {
         if (sDataAgent == null) {
             return;
         }
-        sDataAgent.onResume(null);
+        sDataAgent.onResume(ext);
     }
 
-    public static void onPause(Activity activity) {
+    public static void onPause(Activity activity, Object ext) {
         if (sDataAgent == null) {
             return;
         }
-        sDataAgent.onPause(null);
+        sDataAgent.onPause(ext);
     }
 
-    public static void login(Activity activity, String customParams) {
+    //******************** 帐号 ********************//
 
-    }
-
-    public static void logout(Activity activity, String customParams) {
-    }
-
-    public static void pay(Activity activity, PayInfo payInfo,
-            PayCallBack payCallBack) {
-    }
-
-    public static void switchAccount(Activity activity, String customParams) {
-    }
-
-    public static void exit(Activity activity, ExitCallBack exitCallBack,
-            String customParams) {
-    }
-
-    public static void openUserCenter(Activity activity) {
-    }
-
-    public static void onCreateRole(Activity activity, RoleInfo role) {
-    }
-
-    public static void onEnterGame(Activity activity, XGUser user,
-            RoleInfo role, GameServerInfo server) {
+    /**
+     *
+     * 创建帐号
+     *
+     * @param activity
+     * @param accountId
+     * @param accountName
+     * @param ext
+     */
+    public static void onAccountCreate(Activity activity, String accountId, String accountName, Object ext) {
         if (sDataAgent == null) {
             return;
         }
-        String serverId = server.getServerId();
-        String serverName = server.getServerName();
-        String accountId = user.getUid();
-        String accountName = user.getUserName();
-        String roleId = role.getRoleId();
-        String roleName = role.getRoleName();
-        int roleLevel = role.getLevel();
-        sDataAgent.onAccountLogin(accountId, accountName, null);
-        sDataAgent.onRoleLogin(accountId, accountName, roleId, roleName,
-                serverId, serverName, roleLevel, "login", null);
     }
 
-    public static void onRoleLevelup(Activity activity, XGUser user,
-            RoleInfo role, GameServerInfo server) {
+    /**
+     *
+     * 帐号登录
+     *
+     * @param activity
+     * @param accountId
+     * @param accountName
+     * @param ext
+     */
+    public static void onAccountLogin(Activity activity, String accountId, String accountName, Object ext) {
         if (sDataAgent == null) {
             return;
         }
-        String serverId = server.getServerId();
-        String accountId = user.getUid();
-        String accountName = user.getUserName();
-        String roleId = role.getRoleId();
-        String roleName = role.getRoleName();
-        int level = role.getLevel();
-        sDataAgent.onRoleLevelUp(serverId, accountId, accountName, roleId,
-                roleName, level, null);
     }
 
-    public static void onEvent(Activity activity, XGUser user, RoleInfo role,
-            GameServerInfo server, String eventId, String eventDesc,
-            int eventVal, Map<String, Object> eventBody, String ext) {
+    /**
+     *
+     * 帐号退出
+     *
+     * @param activity
+     * @param accountId
+     * @param accountName
+     * @param ext
+     */
+    public static void onAccountLogout(Activity activity, String accountId, String accountName, Object ext) {
         if (sDataAgent == null) {
             return;
         }
-        String serverId = server.getServerId();
-        String accountId = user.getUid();
-        String accountName = user.getUserName();
-        String roleId = role.getRoleId();
-        String roleName = role.getRoleName();
-        sDataAgent.onEvent(eventId, eventDesc, eventVal, accountId,
-                accountName, eventBody, ext);
     }
 
-    public static void onMissionBegin(Activity activity, XGUser user,
-            RoleInfo roleInfo, GameServerInfo serverInfo, String missionName,
-            String ext) {
-        onRoleMission(activity, user, roleInfo, serverInfo, missionName,
-                "enter", ext);
+    //******************** 角色 ********************//
+
+    /**
+     *
+     * 角色创建
+     *
+     * @param activity
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onRoleCreate(Activity activity, String serverId, String serverName, String accountId,
+                                    String accountName, String roleId, String roleName, int roleLevel, Object ext) {
     }
 
-    public static void onMissionSuccess(Activity activity, XGUser user,
-            RoleInfo roleInfo, GameServerInfo serverInfo, String missionName,
-            String ext) {
-        onRoleMission(activity, user, roleInfo, serverInfo, missionName,
-                "success", ext);
+    /**
+     *
+     * 角色登录
+     *
+     * @param activity
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onRoleLogin(Activity activity, String serverId, String serverName, String accountId,
+                                   String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+
     }
 
-    public static void onMissionFail(Activity activity, XGUser user,
-            RoleInfo roleInfo, GameServerInfo serverInfo, String missionName,
-            String ext) {
-        onRoleMission(activity, user, roleInfo, serverInfo, missionName,
-                "failed", ext);
+    /**
+     *
+     * 角色登出
+     *
+     * @param activity
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onRoleLogout(Activity activity, String serverId, String serverName, String accountId,
+                        String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+
     }
 
-    private static void onRoleMission(Activity activity, XGUser user,
-            RoleInfo role, GameServerInfo server, String missionName,
-            String missionFlag, String ext) {
+    /**
+     *
+     * 角色进入游戏
+     *
+     * @param activity
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onRoleEnterGame(Activity activity, String serverId, String serverName, String accountId,
+                                       String accountName, String roleId, String roleName, int roleLevel, Object ext) {
         if (sDataAgent == null) {
             return;
         }
-        String serverId = server.getServerId();
-        String accountId = user.getUid();
-        String accountName = user.getUserName();
-        String roleId = role.getRoleId();
-        String roleName = role.getRoleName();
-        int roleLevel = role.getLevel();
-        sDataAgent.onRoleMission(serverId, accountId, accountName, roleId,
-                roleName, roleLevel, missionName, missionFlag, ext);
 
     }
 
+    public static void onRoleLevelUp(Activity activity, String serverId, String serverName, String accountId,
+                                     String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+        if (sDataAgent == null) {
+            return;
+        }
+
+    }
+
+    //********************* 任务 ************************//
+
+    /**
+     *
+     * 任务开始
+     *
+     * @param activity
+     * @param missionId 不可为空,不可为空串
+     * @param missionName
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onMissionBegin(Activity activity, String missionId, String missionName,
+                                      String serverId, String serverName, String accountId,
+                                      String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+
+    }
+
+    /**
+     *
+     * 任务完成
+     *
+     * @param activity
+     * @param missionId 不可为空,不可为空串
+     * @param missionName
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onMissionSuccess(Activity activity, String missionId, String missionName,
+                                        String serverId, String serverName, String accountId,
+                                        String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+    }
+
+    /**
+     *
+     * 任务失败
+     *
+     * @param activity
+     * @param missionId 不可为空,不可为空串
+     * @param missionName
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onMissionFail(Activity activity, String missionId, String missionName,
+                                     String serverId, String serverName, String accountId,
+                                     String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+    }
+
+    //********************* 关卡 ************************//
+
+    /**
+     *
+     * 关卡开始
+     *
+     * @param activity
+     * @param levelsId 不可为空,不可为空串
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onLevelsBegin(Activity activity, String levelsId, String serverId, String serverName,
+                                     String accountId, String accountName, String roleId, String roleName,
+                                     int roleLevel, Object ext) {
+
+    }
+
+
+    /**
+     *
+     * 关卡完成
+     *
+     * @param activity
+     * @param levelsId 不可为空,不可为空串
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onLevelsSuccess(Activity activity, String levelsId, String serverId, String serverName, String accountId,
+                                       String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+
+    }
+
+    /**
+     *
+     * 关卡失败
+     *
+     * @param activity
+     * @param levelsId 不可为空,不可为空串
+     * @param reason
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onLevelsFail(Activity activity, String levelsId, String reason, String serverId, String serverName, String accountId,
+                                    String accountName, String roleId, String roleName, int roleLevel, Object ext) {
+
+    }
+
+    //********************* 道具 ************************//
+
+    /**
+     *
+     * 购买道具
+     *
+     * @param activity
+     * @param itemId 不可为空,不可为空串
+     * @param itemName
+     * @param itemCount
+     * @param listPrice
+     * @param transPrice
+     * @param payGold
+     * @param payBindingGold
+     * @param curGold
+     * @param curBindingGold
+     * @param totalGold
+     * @param totalBindingGold
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onItemBuy(Activity activity, String itemId, String itemName, int itemCount, int listPrice,
+                                 int transPrice, int payGold, int payBindingGold, int curGold, int curBindingGold,
+                                 int totalGold, int totalBindingGold, String serverId, String serverName,
+                                 String accountId, String accountName, String roleId, String roleName, int roleLevel,
+                                 Object ext) {
+
+    }
+
+    /**
+     *
+     * 获取道具
+     *
+     * @param activity
+     * @param itemId 不可为空,不可为空串
+     * @param itemName
+     * @param itemCount
+     * @param listPrice
+     * @param transPrice
+     * @param payGold
+     * @param payBindingGold
+     * @param curGold
+     * @param curBindingGold
+     * @param totalGold
+     * @param totalBindingGold
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    public static void onItemGet(Activity activity, String itemId, String itemName, int itemCount, int listPrice,
+                                 int transPrice, int payGold, int payBindingGold, int curGold, int curBindingGold,
+                                 int totalGold, int totalBindingGold, String serverId, String serverName,
+                                 String accountId, String accountName, String roleId, String roleName, int roleLevel,
+                                 Object ext) {
+
+    }
+
+    /**
+     *
+     * 消耗道具
+     *
+     * @param activity
+     * @param itemId 不可为空,不可为空串
+     * @param itemName
+     * @param itemCount
+     * @param listPrice
+     * @param transPrice
+     * @param payGold
+     * @param payBindingGold
+     * @param curGold
+     * @param curBindingGold
+     * @param totalGold
+     * @param totalBindingGold
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    // TODO: 参数待定
+    public static void onItemConsume(Activity activity, String itemId, String itemName, int itemCount, int listPrice,
+                                     int transPrice, int payGold, int payBindingGold, int curGold, int curBindingGold,
+                                     int totalGold, int totalBindingGold, String serverId, String serverName,
+                                     String accountId, String accountName, String roleId, String roleName, int roleLevel,
+                                     Object ext) {
+
+    }
+
+    //********************* 金币 ************************//
+
+    /**
+     *
+     * 获取金币
+     *
+     * @param activity
+     * @param gainChannel
+     * @param gold
+     * @param bindingGold
+     * @param curGold
+     * @param curBindingGold
+     * @param totalGold
+     * @param totalBindingGold
+     * @param serverId
+     * @param serverName
+     * @param accountId
+     * @param accountName
+     * @param roleId
+     * @param roleName
+     * @param roleLevel
+     * @param ext
+     */
+    // TODO: 参数待定
+    public static void onGoldGain(Activity activity, String gainChannel, int gold, int bindingGold, int curGold,
+                                  int curBindingGold, int totalGold, int totalBindingGold, String serverId, String serverName,
+                                  String accountId, String accountName, String roleId, String roleName, int roleLevel,
+                                  Object ext) {
+
+    }
+
+    //********************* 自定义事件 ************************//
+
+    public static void onEvent(Activity activity, String eventId, String eventDesc, String serverId, String serverName,
+                               String accountId, String accountName, int eventVal, Map<String, Object> eventBody, Object ext) {
+        if (sDataAgent == null) {
+            return;
+        }
+    }
 }
